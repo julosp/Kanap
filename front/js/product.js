5703e63fd5b7460b11897d4eb109b96769205b19
Julien Ospital <ospital.julien@gmail.com>
2022-08-18 18:43:35 +0200

    fetch product by id (3000 de QI )

--- front/js/product.js
+++ front/js/product.js
@@ -1,16 +1,37 @@
-
-
-
-async function getProducts() {
-  let api = await fetch ("http://localhost:3000/api/products");
-  let products = await api.json();
-  console.log(products)
+async function idByUrl() {
+  var url = window.location.search;
+  url = url.replace("?_id", "");
+  return url
 }
 
-async function idByUrl() {
-    let url = location.href;
-    let id = url.substring(url.lastIndexOf("id") + 2);
-    console.log(id)
-  }
+async function getProducts() {
+  let id = await idByUrl()
+  let url = "http://localhost:3000/api/products/" + id;
+  console.log(url)
+  fetch(url)
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+  })
+  .catch(function(err) {
+  });
+}
 
-  console.log(getProducts())
+async function renderProduct(){
+  let product = await getProducts();
+  let newHtml = ''
+}
+
+
+
+
+
+
+
+
+
+console.log(renderProduct());
