8391fa5e7fe40601da47f4db4af4fa7d89b61117
Julien Ospital <ospital.julien@gmail.com>
2022-08-24 18:20:53 +0200

    link api to product + insertion in html

--- front/js/product.js
+++ front/js/product.js
@@ -1,29 +1,44 @@
+/*ISOLER L'ID DU PRODUIT*/ 
 async function idByUrl() {
   var url = window.location.search;
   url = url.replace("?_id", "");
-  return url
+  return url;
 }
-
+/*FETCH L'API ET AJOUTER L'ID ISOLER*/ 
 async function getProducts() {
-  let id = await idByUrl()
+  let id = await idByUrl();
   let url = "http://localhost:3000/api/products/" + id;
-  console.log(url)
-  fetch(url)
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(value) {
-    console.log(value);
-  })
-  .catch(function(err) {
-  });
+  try {
+    let res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+/*RECUPERATION DU PRODUIT DEPUIS L'API
+CREATION IMG ET AJOUT D'ATTRIBUTS
+INSERTION IMG DANS LE HTML*/
+async function renderImg() {
+  let products = await getProducts();
+  let htmlSegment = document.createElement("img");
+  let container = document.getElementById('item__img');
+  htmlSegment.setAttribute("src",products.imageUrl)
+  htmlSegment.setAttribute("alt",products.altTxt);
+  container.appendChild(htmlSegment)
 }
 
-async function renderProduct(){
-  let product = await getProducts();
-  let newHtml = ''
+/*RECUPERATION DU PRODUIT DEPUIS L'API
+INSERTION DU NAME DANS LE HTML*/
+async function renderName() {
+  let products = await getProducts();
+  let container = document.getElementById('title');
+  container.innerHTML = products.name
+}
+
+async function renderPrice() {
+  let products = await getProducts();
+  let container = document.getElementById('price');
+  container.innerHTML = products.price
 }
 
 
@@ -34,4 +49,6 @@ async function renderProduct(){
 
 
 
-console.log(renderProduct());
+console.log(renderPrice())
+console.log(renderName())
+console.log(renderImg());
