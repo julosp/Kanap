f2482608d10db6198da190e855720ef695c362c7
Julien Ospital <ospital.julien@gmail.com>
2022-08-29 10:47:21 +0200

    push product in array + check if array is valid

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,10 @@
-/*ISOLER L'ID DU PRODUIT*/ 
+/*ISOLER L'ID DU PRODUIT*/
 async function idByUrl() {
   var url = window.location.search;
   url = url.replace("?_id", "");
   return url;
 }
-/*FETCH L'API ET AJOUTER L'ID ISOLER*/ 
+/*FETCH L'API ET AJOUTER L'ID ISOLER*/
 async function getProducts() {
   let id = await idByUrl();
   let url = "http://localhost:3000/api/products/" + id;
@@ -21,34 +21,117 @@ INSERTION IMG DANS LE HTML*/
 async function renderImg() {
   let products = await getProducts();
   let htmlSegment = document.createElement("img");
-  let container = document.getElementById('item__img');
-  htmlSegment.setAttribute("src",products.imageUrl)
-  htmlSegment.setAttribute("alt",products.altTxt);
-  container.appendChild(htmlSegment)
+  let container = document.getElementById("item__img");
+  htmlSegment.setAttribute("src", products.imageUrl);
+  htmlSegment.setAttribute("alt", products.altTxt);
+  container.appendChild(htmlSegment);
 }
 
 /*RECUPERATION DU PRODUIT DEPUIS L'API
 INSERTION DU NAME DANS LE HTML*/
 async function renderName() {
   let products = await getProducts();
-  let container = document.getElementById('title');
-  container.innerHTML = products.name
+  let container = document.getElementById("title");
+  container.innerHTML = products.name;
 }
 
+/*RECUPERATION DU PRODUIT DEPUIS L'API
+INSERTION DU NAME DANS LE HTML*/
 async function renderPrice() {
   let products = await getProducts();
-  let container = document.getElementById('price');
-  container.innerHTML = products.price
+  let container = document.getElementById("price");
+  container.innerHTML = products.price;
 }
 
+/*RECUPERATION DU PRODUIT DEPUIS L'API
+INSERTION DU NAME DANS LE HTML*/
+async function renderDescription() {
+  let products = await getProducts();
+  let container = document.getElementById("description");
+  container.innerHTML = products.description;
+}
+
+/*RECUPERATION DU PRODUIT DEPUIS L'API
+ARRAY COLOR DANS UN VARIABLE
+LOOP DANS LES COULEURS
+CREATION OPT + AJOUT TEXT ET VALUE*/
+async function renderColor() {
+  let products = await getProducts();
+  let colorsArray = products.colors;
+  let select = document.getElementById("colors");
+  for (var i = 0; i < colorsArray.length; i++) {
+    let option = colorsArray[i];
+    let newOption = document.createElement("option");
+    newOption.textContent = option;
+    newOption.value = option;
+    select.appendChild(newOption);
+  }
+}
+
+async function renderAll(){
+  renderPrice();
+  renderName();
+  renderImg();
+  renderDescription();
+  renderColor();
+}
+console.log(renderAll())
+
+cartBtn = document.getElementById('addToCart')
+cartBtn.addEventListener("click", addToCart)
+
+async function addToCart() {
+  //RECUP ID
+  let product = await getProducts();
+  let productId = product._id;
+  //RECUP COLOR
+  //CHECK IF COLOR IS VALID
+  function checkColor(){
+    let select = document.getElementById('colors');
+    let colorValue = select.value
+    if (colorValue === ""){
+      //alert("Veuillez choissir une couleur")
+      return false
+    } else {
+      return colorValue
+    }
+  }
+ 
+  //RECUP QUANTITY
+  //CHECK IF QUANTITY IS VALID
+  function checkQuantity(){
+    let quantityInput = document.getElementById('quantity')
+    let quantity = quantityInput.value
+    if (quantity === "0"){
+      //alert("Veuillez ajouter un nombre d'article")
+      return false
+    } else if (quantity < "0"){
+      //alert("Veuillez ajouter un nombre d'article valide")
+      return false
+    } else {
+      return quantity
+    }
+  }
+
+
+  class productInCart {
+    constructor(id,color,quantity){
+      this.id = id;
+      this.color = color;
+      this.quantity = quantity;
+    }
+  }
+  let productArray = new productInCart(productId,checkColor(),checkQuantity())
+  let areFalsy = Object.values(productArray).every(value => value);
+  if (areFalsy === false){
+    console.log("non")
+  } else {
+    console.log("top ")
+  }
+  
+  }
+  
+  
 
 
 
-
-
-
-
-
-console.log(renderPrice())
-console.log(renderName())
-console.log(renderImg());
